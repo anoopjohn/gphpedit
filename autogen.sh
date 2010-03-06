a4d572f9a941874a396dd01c0bcedfaa6b2c6cea
Jose Rostagno <joserostagno@hotmail.com>
2010-03-06 18:21:08 -0300

    fix gettext issue

--- autogen.sh
+++ autogen.sh
@@ -18,6 +18,6 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 #
-autoreconf -v -i
+autoreconf -v -i -f
 echo Running configure with arguments "$@"
 ./configure $@
