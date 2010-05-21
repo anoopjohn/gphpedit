7327ff6e81881e43f27e98b8d7bf29126b68d18a
Jose Rostagno <joserostagno@hotmail.com>
2010-05-21 19:50:48 -0300

    tab: fix memory leak in save code

--- src/tab.c
+++ src/tab.c
@@ -234,14 +234,19 @@ static void tab_set_event_handlers(Editor *editor)
   g_signal_connect (G_OBJECT (editor->scintilla), "update_ui", G_CALLBACK (update_ui), NULL);
 }
 
+gchar *write_buffer = NULL; /*needed for save buffer*/
+
 void tab_file_write (GObject *source_object, GAsyncResult *res, gpointer user_data)
 {
   Editor *editor = (Editor *)user_data;
   GError *error=NULL;
   if(!g_file_replace_contents_finish ((GFile *)source_object,res,NULL,&error)){
     g_print(_("GIO Error: %s saving file:%s\n"),error->message,editor->filename->str);
+    g_free(write_buffer);
+    g_error_free(error);
     return;
   }
+  g_free(write_buffer);
   gtk_scintilla_set_save_point (GTK_SCINTILLA(editor->scintilla));
   GFileInfo *info;
   info= g_file_query_info ((GFile *)source_object,"time::modified,time::modified-usec",G_FILE_QUERY_INFO_NONE, NULL,&error);
@@ -260,7 +265,6 @@ void tab_file_write (GObject *source_object, GAsyncResult *res, gpointer user_da
 
 void tab_file_save_opened(Editor *editor,GFile *file)
 {
-  gchar *write_buffer = NULL;
   gsize text_length;
   GError *error = NULL;
   gchar *converted_text = NULL;
