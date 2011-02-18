5412c4802689750402667dd18f9735bf664c3ad4
Jose Rostagno <joserostagno@vijona.com.ar>
2011-02-18 11:51:01 -0300

    Add license notice in new files

--- src/documentable.c
+++ src/documentable.c
@@ -1,3 +1,26 @@
+/* This file is part of gPHPEdit, a GNOME PHP Editor.
+
+   Copyright (C) 2011 José Rostagno (for vijona.com.ar)
+
+   For more information or to find the latest release, visit our 
+   website at http://www.gphpedit.org/
+
+   gPHPEdit is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   gPHPEdit is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with gPHPEdit. If not, see <http://www.gnu.org/licenses/>.
+
+   The GNU General Public License is contained in the file COPYING.
+*/
+
 #include "documentable.h"
 
 G_DEFINE_INTERFACE(Documentable, documentable, G_TYPE_INVALID)
